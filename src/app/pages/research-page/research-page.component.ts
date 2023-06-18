294f0cbb211d3b784d35f76a2517a0b4695dd4ba
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-06-18 14:43:35 +0530

    Need to write category and author tags

--- src/app/pages/research-page/research-page.component.ts
+++ src/app/pages/research-page/research-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ResearchData, publication, researchPage } from 'src/data/research';
 
 @Component({
@@ -6,7 +6,39 @@ import { ResearchData, publication, researchPage } from 'src/data/research';
   templateUrl: './research-page.component.html',
   styleUrls: ['./research-page.component.css']
 })
-export class ResearchPageComponent {
+export class ResearchPageComponent implements OnInit{
   data:researchPage = ResearchData;
-
+  categories:string[] = [];
+  query:string=``;
+  getQueryResults(): publication[] {
+    const lowerCaseQuery = this.query.toLowerCase();
+    return this.data.papers.filter((paper)=>this.paperQueryMatch(paper,lowerCaseQuery))
+  }
+  ngOnInit(){
+    //optimize this:
+    for(let paper of this.data.papers){
+      for(let category of paper.categories){
+        if(!this.categories.includes(category)){
+          this.categories.push(category)
+        }
+      }
+    }
+  }
+  getPapersByCategory(category:string):publication[]{
+    return this.data.papers.filter(paper=>(paper.categories.includes(category)))
+  }
+  paperQueryMatch(paper:publication,lowerCaseQuery:string):boolean{
+    if(
+      paper.title.toLowerCase().includes(lowerCaseQuery)
+      ||
+      paper.authors.join(',').toLowerCase().includes(lowerCaseQuery)
+      ||
+      paper.description.toLowerCase().includes(lowerCaseQuery)
+      ||
+      paper.categories.join(',').toLowerCase().includes(lowerCaseQuery)
+      ){
+      return true;
+    }
+    return false;
+  }
 }
