f8527e512a8a4eb2ceeae629926a6e3354e8bcbe
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-07-01 17:55:27 +0530

    search bar

--- src/app/pages/events-page/events-page.component.ts
+++ src/app/pages/events-page/events-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { EventsData, eventsPageSpec } from 'src/data/events';
+import { EventSpec, EventsData, eventsPageSpec } from 'src/data/events';
+import { getParaSpecStr } from 'src/data/rtl.utils';
 
 @Component({
   selector: 'app-events-page',
@@ -8,4 +9,35 @@ import { EventsData, eventsPageSpec } from 'src/data/events';
 })
 export class EventsPageComponent {
   data:eventsPageSpec = EventsData;
+  query:string = '';
+  getQueryResults(): EventSpec[] {
+    const lowerCaseQuery = this.query.toLowerCase();
+    return [
+      ...this.data.forthcomingEvents.filter((seminar)=>this.eventQueryMatch(seminar,lowerCaseQuery)),
+      ...this.data.pastEvents.filter((event)=>this.eventQueryMatch(event,lowerCaseQuery))
+    ]
+  }
+  eventQueryMatch(event: EventSpec, lowerCaseQuery: string): boolean {
+    if(
+      event.title.toLowerCase().includes(lowerCaseQuery)
+      ||
+      event.description.map(para=>getParaSpecStr(para)).join('.').toLowerCase().includes(lowerCaseQuery)
+      ||
+      event.venue.toLowerCase().includes(lowerCaseQuery)
+      ||
+      event.dateTime.toLowerCase().includes(lowerCaseQuery)
+      ){
+        return true;
+    }
+    if(event.contact){
+      return event.contact.toLowerCase().includes(lowerCaseQuery)
+    }
+    if(event.subtitle){
+      return event.subtitle.toLowerCase().includes(lowerCaseQuery)
+    }
+    if(event.moreDetails){
+      return event.moreDetails.map(para=>getParaSpecStr(para)).join('.').toLowerCase().includes(lowerCaseQuery)
+    }
+    return false
+  }
 }
