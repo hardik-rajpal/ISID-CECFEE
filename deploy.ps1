f20a11410eac2db2d74f203295d943c5fe615a19
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-07-27 00:21:34 +0530

    base url fix

--- deploy.ps1
+++ deploy.ps1
@@ -2,7 +2,7 @@ bash -c 'rm -rf .angular node_modules/.cache dist'
 git checkout deploy
 git merge master
 bash -c 'rm main.*.js polyfills.*.js runtime.*.js styles.*.css index.html'    
-ng build --base-href https://webdev-il.github.io/ISID-CECFEE/;
+ng build --base-href https://hardik-rajpal.github.io/ISID-CECFEE/;
 bash -c 'mv dist/isid-cecfee/* .';
 bash -c 'cp index.html 404.html; rm -rf assets; cp -r src/assets assets';
 git add -A;
