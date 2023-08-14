110ad33b5c76c28c5e98928163fc78e82f2e8a5c
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-08-14 10:12:48 +0530

    long over due style fixes.

--- src/app/comps/updates-carousel/updates-carousel.component.ts
+++ src/app/comps/updates-carousel/updates-carousel.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
+import { MdbCarouselComponent } from 'mdb-angular-ui-kit/carousel';
 import { updatesSpec } from 'src/data/homepage';
 import { infoTileSpec } from 'src/data/rtl.utils';
 
@@ -9,4 +10,33 @@ import { infoTileSpec } from 'src/data/rtl.utils';
 })
 export class UpdatesCarouselComponent {
   @Input() updates!:updatesSpec;
+  @ViewChild('carousel') carousel!:MdbCarouselComponent;
+  slideNumbers:number[] = []
+  forward(){
+    this.carousel.next()
+  }
+  backward(){
+    this.carousel.prev()
+  }
+  pause(){
+    this.carousel.stop()
+  }
+  play(){
+    this.carousel.play()
+  }
+  getActiveSlide(){
+    if(this.carousel){
+      return this.carousel.activeSlide
+    }
+    return 0;
+  }
+  ngOnInit(){
+    let numSlides = this.updates.events.length + this.updates.infotiles.length;
+    for(let i=0;i<numSlides;i++){
+      this.slideNumbers.push(i);
+    }
+  }
+  setSlide(index:number){
+    this.carousel.to(index);
+  }
 }
