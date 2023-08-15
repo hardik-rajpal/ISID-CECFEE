51893987a5cb9d29f16eb247c437302eb04d6ab5
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-08-15 10:17:58 +0530

    not right minheight work.

--- src/app/comps/updates-carousel/updates-carousel.component.ts
+++ src/app/comps/updates-carousel/updates-carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, Input, Renderer2, ViewChild } from '@angular/core';
 import { MdbCarouselComponent } from 'mdb-angular-ui-kit/carousel';
 import { updatesSpec } from 'src/data/homepage';
 import { infoTileSpec } from 'src/data/rtl.utils';
@@ -11,7 +11,9 @@ import { infoTileSpec } from 'src/data/rtl.utils';
 export class UpdatesCarouselComponent {
   @Input() updates!:updatesSpec;
   @ViewChild('carousel') carousel!:MdbCarouselComponent;
-  slideNumbers:number[] = []
+  slideNumbers:number[] = [];
+  interval = 2500;
+  constructor(private renderer:Renderer2){}
   forward(){
     this.carousel.next()
   }
@@ -35,6 +37,35 @@ export class UpdatesCarouselComponent {
     for(let i=0;i<numSlides;i++){
       this.slideNumbers.push(i);
     }
+  }
+  infotileQS = 'div.infotilewrapper';
+  minHeight = 0;
+  infotilewrapperStyle = '';
+  heights:number[] = [];
+  updateHeight(){
+    let currHeight  = this.carousel.items[this.carousel.activeSlide].host.clientHeight;
+    this.heights.push(currHeight);
+    console.log(this.heights);
+    if(this.minHeight < currHeight){
+      this.minHeight = currHeight;
+      this.infotilewrapperStyle = `min-height:${this.minHeight}`
+      let infotilewrappers = document.querySelectorAll(this.infotileQS);
+      console.log(infotilewrappers)
+      // infotilewrappers.forEach((infotilewrapper)=>{
+      //   this.renderer.setStyle(infotilewrapper,'minHeight',currHeight);
+      // })
+      console.log('setting minheight to ',currHeight);
+    }
+    if(this.heights.length < this.slideNumbers.length){
+      setTimeout(()=>{
+        this.updateHeight()
+      },this.interval)
+    }
+  }
+  ngAfterViewInit(){
+    setTimeout(()=>{
+      this.updateHeight();
+    },(this.interval*0.75))
   }
   setSlide(index:number){
     this.carousel.to(index);
