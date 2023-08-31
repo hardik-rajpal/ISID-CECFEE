bfa1daab81ffd536737dc584fe906121a65b2927
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-08-31 12:55:53 +0530

    powershell script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
-#copy from epu
-#include staticfs
+if [ "$#" -ne 1 ]; then
+    echo "Usage: bash deploy.sh <baseURL>. Ex: bash deploy.sh https://www.isid.ac.in/~testdeploy/"
+    exit
+fi
+rm -rf .angular node_modules/.cache dist deploy
+ng build --base-href "$1";
+cp -r "dist/isid-cecfee/" ./deploy
